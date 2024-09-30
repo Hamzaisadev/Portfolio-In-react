846191fab344a538d7a84033dfb358d3691a545c
HamzaIshaq <156201157+Hamzaisadev@users.noreply.github.com>
2024-10-01 01:42:25 +0500

    1.5

--- react portfolio/src/components/Hero/Hero.jsx
+++ react portfolio/src/components/Hero/Hero.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import {getUrlImage} from "../../utils"
+import Styles from './Hero.module.css'
 
 export const Hero = () => {
   return (
-  <section>
-  <div>
-    <h1>Hi, I'am Hamza</h1>
-    <p>I'm a Web Developer proficient in HTML, CSS, JavaScript, Python, and increasingly skilled in
+  <section className={Styles.container}>
+  <div className={Styles.content}>
+    <h1 className={Styles.title}>Hi, I'am Hamza</h1>
+    <p className={Styles.description}>I'm a Web Developer proficient in HTML, CSS, JavaScript, Python, and increasingly skilled in
   React. With over 20 successful web projects developed and deployed,</p>
-    <a href="../../../assets/resume/resume.pdf" download="hamza's resume">Download Resume</a>
+    <div className={Styles.btnContainer}>
+      <a href="../../../assets/resume/resume.pdf" className={Styles.resumeBtn} download="hamza's resume">Resume</a>
+      <a href="mailto:hamzaisadev@gmail.com" className={Styles.contactBtn} >Contact Me</a>
+    </div>
   </div>
-  {/* <img src={getUrlImage("hero/heroImage2.png")} alt="" /> */}
+  { <img src={getUrlImage("hero/heroImage2.png")} alt="ffasfef" className={Styles.heroImg} />  }
+  <div className={Styles.topBlur} /  >
+  <div className={Styles.bottomBlur} />
 
   </section>
   )
